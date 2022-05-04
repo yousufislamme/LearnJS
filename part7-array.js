12b8ab1c7bd98aeea5859c86f59745da84333338
yousufislamme <online.okk@gmail.com>
2022-05-05 00:03:32 +0600

    some modify

--- part7-array.js
+++ part7-array.js
@@ -1,9 +1,9 @@
 var friendAge = [12, 14, 20, 77, 32, 18];
 friendAge.push(26);
-friendAge = friendAge.unshift('jalam');
+friendAge == friendAge.unshift('jalam');
 // var position = friendAge.indexOf(20);
 
 console.log(friendAge.length);
-console.log( friendAge);
+console.log(friendAge);
 var buyerRequest = 215;
 var perDay = 10;
