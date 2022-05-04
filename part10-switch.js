27458e8478ecc18987ffbaad204b259532cfa32f
yousufislamme <online.okk@gmail.com>
2022-05-05 00:04:22 +0600

    modify

---
+++ part10-switch.js
@@ -0,0 +1,17 @@
+var num = 100;
+switch (num) {
+   case 1000:
+      console.log("I'm 1000");
+      break;
+
+   case 100:
+      console.log("I'm 100");
+      break;
+   
+   case 2:
+      console.log("I'm 20");
+      break;
+   default:
+      console.log("I don't know how are");
+      break;
+}
---
+++ part9-forloop.js
@@ -0,0 +1,17 @@
+// for (i=1; i<=10;i++) {
+//    console.log(i);
+// }
+
+var friendAge = [14, 18, 16, 20, 25, 16];
+
+for (var i = 0; i < friendAge.length; i++) {
+   var element = friendAge[i]
+   console.log(element);
+}
+
+
+
+
+
+
+
