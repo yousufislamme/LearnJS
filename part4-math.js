f2d53d4a7f2aebc640cae22b522079cae0f35e5e
yousufislamme <online.okk@gmail.com>
2022-05-04 13:57:07 +0600

    Math absolute round floor ceil random

---
+++ part4-math.js
@@ -0,0 +1,19 @@
+// Math absolute round floor ceil random
+
+var number1 = 0.1;
+var number2 = 0.2;
+
+var result = number1 + number2;
+result = Math.abs(result);
+result = result.toFixed(2);
+
+// console.log(result);
+ 
+
+var randomNumber = Math.random() * 100;
+var randomResult = Math.round(randomNumber);
+
+
+
+
+console.log(randomResult);
