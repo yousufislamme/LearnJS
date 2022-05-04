c2826862d9275c2321487ae5b5f78998067d3c57
yousufislamme <online.okk@gmail.com>
2022-05-04 19:52:42 +0600

    conditional and array

---
+++ part6-MultipleConditions.js
@@ -0,0 +1,8 @@
+var jobPaiso = true;
+var savingAmount = 12000;
+
+if ( jobPaiso == true || savingAmount > 100000 ) {
+   console.log("congratulation");
+} else {
+   console.log("sorry");
+}
---
+++ part7-array.js
@@ -0,0 +1,7 @@
+var friendAge = [12, 14, 20, 77, 32, 18];
+friendAge.push(26);
+friendAge = friendAge.unshift('jalam');
+// var position = friendAge.indexOf(20);
+
+console.log(friendAge.length);
+console.log( friendAge);
