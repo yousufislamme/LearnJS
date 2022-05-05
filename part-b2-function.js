67713173018dc0d7f77cab016d54e787871e39a6
yousufislamme <online.okk@gmail.com>
2022-05-05 12:42:57 +0600

    function of JavaScript

---
+++ part-b2-function.js
@@ -0,0 +1,30 @@
+function friendAge(yousuf, islam, ibrahim) {
+   var ourAgeResult = yousuf + islam + ibrahim;
+   return ourAgeResult;
+}
+
+var totalAge = friendAge(22, 18, 45);
+
+console.log(totalAge);
+
+
+function number(num1, num2) {
+   var result = num1 + num2;
+   return result;
+
+}
+
+var push = number(7, 11);
+
+console.log(push);
+
+// Eid Bonus 2x
+
+function eidBonus(tk){
+   var doubleMoney = tk * 2;
+   return doubleMoney;
+}
+
+var eidSalami = eidBonus(500);
+
+console.log(eidSalami);
